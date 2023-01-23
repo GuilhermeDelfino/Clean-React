b780de5f13cf963e8d54a71f17663e54abf241b2
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-01-22 21:03:35 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import('jest').Config;}
+ */
+const config = {
+	roots: ['<rootDir>/src'],
+	collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+	coverageDirectory: 'coverage',
+	testEnvironment: 'jsdom',
+	transform: {
+		'.+\\.ts$': 'ts-jest'
+	}
+};
+
+export default config;
